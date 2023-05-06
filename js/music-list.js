b48bc75ea05827eef78b78d0367c8fd5a5b21234
Neha-Haroon <90547706+Neha-Haroon@users.noreply.github.com>
2023-05-06 11:50:31 +0500

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,33 @@
+let allMusic = [
+    {
+        name: "Alone",
+        artist: "Alan Walker",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Faded",
+        artist: "Alan Walker",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Bad Liar",
+        artist: "Imagine Dragons",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Iridiescent",
+        artist: "Linkin Park",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "In the End",
+        artist: "Linkin Park",
+        img: "music-5",
+        src: "music-5"
+    },
+
+];
